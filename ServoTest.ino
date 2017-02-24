e241fcb0b2dd900c2827433a5bec816c8ad851b7
kennyevanadams <kennyevanadams@gmail.com>
2017-02-24 09:19:33 -0500

    Updated ServoTest.ino to switch directions.

--- ServoTest.ino
+++ ServoTest.ino
@@ -1,6 +1,9 @@
 // defines pins numbers
 const int stepPin = 6; 
 const int dirPin = 5; 
+//defines pause in between movements
+const int delBetweenSteps = 500;
+const int delBetweenSets =1000;
  
 void setup() {
   // Sets the two pins as Outputs
@@ -8,13 +11,23 @@ void setup() {
   pinMode(dirPin,OUTPUT);
 }
 void loop() {
-  digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
+  digitalWrite(dirPin,HIGH); // Enables the motor to move in dir 1.
   // Makes 200 pulses for making one full cycle rotation
   for(int x = 0; x < 200; x++) {
     digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(500); 
+    delayMicroseconds(delBetweenSteps); 
     digitalWrite(stepPin,LOW); 
-    delayMicroseconds(500); 
+    delayMicroseconds(delBetweensteps); 
   }
-  delay(1000); // One second delay
+  delay(delBetweenSets); // One second delay
+ 
+  digitalWrite(dirPin,LOW); // Enables the motor to move in dir 2.
+  // Makes 200 pulses for making one full cycle rotation
+  for(int x = 0; x < 200; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(delBetweenSteps); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(delBetweensteps); 
+  }
+  delay(delBetweenSets); // One second delay
 }
